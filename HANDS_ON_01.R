2d1176a65e2755eab1df12db51c894715317998c
Hugo De Francisco Benito <hugo.defrancisco.benito@gmail.com>
2023-09-22 09:38:55 +0200

    Installed libs, load data, basic git and r commands

--- HANDS_ON_01.R
+++ HANDS_ON_01.R
@@ -1,6 +1,41 @@
 
-# SCRIPT + STUDENT INFO ---------------------------------------------------
+# SCRPT + STUDENT INFO ----------------------------------------------------
 
-  # NOMBRE: Hugo De Francisco
+  # NOMBRE: Hugo De Francisco Benito
   # EXP:22152945
   # TEMA: HANDS_ON_01
+
+
+# LOADING LIBRARIES -------------------------------------------------------
+  
+  install.packages("tydiverse")
+
+# LOADING DATA ------------------------------------------------------------
+
+  # DPLYR
+
+# SHORTCUTS ---------------------------------------------------------------
+
+  # CLEAN CONSOLE = CRTL + l
+  # %>% = pipe operator -> shift + crtl + m
+  # run line = shift + enter
+
+# GIT COMMANDS ------------------------------------------------------------
+
+  # git status = info about a repo
+  # git commit = add a commit
+  # git add . = add the current dir to the entire repo
+  # git push -u origin main = send to the remote repo (Git hub)
+
+# CLI COMMANDS ------------------------------------------------------------
+
+  # pwd = shows the current location
+  # ls = list terminal
+  # mkdir = create a dir
+  # cd = change dir
+
+# BASIC INSTRUCTIONS ------------------------------------------------------
+
+  hugo<-8 #assigning values
+  
+  
